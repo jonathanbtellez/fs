1e0fcb21c2f2a50c4af629a75dbb83ec8140b7f2
Jonathan Tellez <jonathanbtellez@gmail.com>
2023-11-15 17:16:20 -0500

    1-6

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,65 +1,43 @@
+import { useState } from 'react';
 import './App.css';
 
-const Header = ({ title }) => {
-  return (
-    <div>
-      <h2>{title}</h2>
-    </div>
-  )
-}
+// Este es lugar correcto para definir un componente
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
 
-const Content = (props) => {
-  return (
-    <div>
-      <Part course={props.part.parts[0]} />
-      <Part course={props.part.parts[1]} />
-      <Part course={props.part.parts[2]} />
-    </div>
-  )
-}
+// No defina componentes adentro de otro componente
 
-const Part = (props) => {
-  return (
-    <div>
-      <p>{props.course.name} - {props.course.exercises}</p>
-    </div>
-  )
-}
-
-const Footer = (props) => {
-  return (
-    <div>
-      <p>Number of exercises = {props.exercises.parts[0].exercises + props.exercises.parts[1].exercises + props.exercises.parts[2].exercises}</p>
-    </div>
-  )
-}
+const Display = ({ value, text }) => <div>{text} {value}</div>
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const increaseGood = () => setGood(good + 1)
+  const increaseNeutral = () => setNeutral(neutral + 1)
+  const increaseBad = () => setBad(bad + 1)
+
+
 
   return (
     <div>
-      <Header title={course.name} />
-      <Content part={course} />
-      <Footer exercises={course} />
+      <h1>Give feedback</h1>
+      <Button handleClick={increaseGood} text="Good" />
+      <Button handleClick={increaseNeutral} text="Neutral" />
+      <Button handleClick={increaseBad} text="Bad" />
+
+      <h1>Statistics</h1>
+      <Display value={good} text="Good" />
+      <Display value={neutral} text="Neutral" />
+      <Display value={bad} text="Bad" />
+
+
     </div>
   )
 }
-
 export default App;
---
+++ part1/src/AppComplexState.js
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import './App.css';
+
+// Este es lugar correcto para definir un componente
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+// No defina componentes adentro de otro componente
+
+const Display = props => <div>{props.value}</div>
+
+const App = () => {
+  const [value, setValue] = useState(10)
+
+  const setToValue = newValue => {
+    setValue(newValue)
+  }
+
+
+  return (
+    <div>
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
+    </div>
+  )
+}
+export default App;
