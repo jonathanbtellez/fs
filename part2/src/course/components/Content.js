00067a768c4a4b25c5653f8e1c0e1ea10944180d
Jonathan Tellez <jonathanbtellez@gmail.com>
2023-11-17 11:37:03 -0500

    2.1

---
+++ part2/src/course/components/Content.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Part from './Part'
+
+function Content({ parts }) {
+    return (
+        <ul>
+            {
+                parts.map(part =>
+                    <Part key={part.id} part={part} />
+                )
+            }
+        </ul>
+    )
+}
+
+export default Content
