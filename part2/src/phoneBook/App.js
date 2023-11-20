ef8b75059feffe383d86e060b1b02ae9ec1375bf
Jonathan Tellez <jonathanbtellez@gmail.com>
2023-11-19 20:26:46 -0500

    2.9

--- part2/src/phoneBook/App.js
+++ part2/src/phoneBook/App.js
@@ -2,10 +2,19 @@ import { useState } from "react"
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '12345678' }
+    { name: 'Arto Hellas', number: '12345678' },
+    { name: 'Carlos Hellas', number: '12345678' },
+    { name: 'David Hellas', number: '12345678' },
+    { name: 'Marin Hellas', number: '12345678' },
+    { name: 'Aron Hellas', number: '12345678' },
+    { name: 'Camilo Hellas', number: '12345678' },
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState("")
+  const [newSearch, setNewSearch] = useState("")
+  const [showAll, setshowAll] = useState(true)
+
+
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -14,6 +23,17 @@ const App = () => {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
+
+  const handleSearchChange = (event) => {
+    if (event.target.value.length > 0) {
+      setshowAll(false)
+    } else {
+      setshowAll(true)
+    }
+    setNewSearch(event.target.value)
+  }
+
+  const filteredContacts = showAll ? persons : persons.filter(person => person.name.toLowerCase().includes(newSearch.toLowerCase()))
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -27,12 +47,13 @@ const App = () => {
     setPersons([...persons, { name: newName, number: newNumber }])
     setNewName("")
     setNewNumber("")
-
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      Filter contacts: <input value={newSearch} onChange={handleSearchChange} />
+
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -47,7 +68,8 @@ const App = () => {
       ...
       <div>
         <ul>
-          {persons.map(person => <li key={person.name}>{person.name} - {person.number}</li>)}
+
+          {filteredContacts.length > 0 ? filteredContacts.map(person => <li key={person.name}>{person.name} - {person.number}</li>) : <p>Not contacts find</p>}
         </ul>
       </div>
     </div>
