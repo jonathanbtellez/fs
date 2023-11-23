fa213564e892f68e9992c31a3c7646a72da8859f
Jonathan Tellez <jonathanbtellez@gmail.com>
2023-11-23 13:26:46 -0500

    fix issue processing close answer

--- part2/src/countries/components/Country.js
+++ part2/src/countries/components/Country.js
@@ -1,19 +1,39 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 function Country({ country }) {
+
+    const [weather, setWeather] = useState(null)
+
     let lenguagesList = []
     let lenguagesKeys = country ? Object.keys(country.languages) : [];
     for (let i = 0; i < lenguagesKeys.length; i++) {
         let key = lenguagesKeys[i];
         lenguagesList = [...lenguagesList, country.languages[key]];
     }
+    useEffect(() => {
+        if (country)
+            axios
+                .get('http://api.weatherstack.com/current', {
+                    params: {
+                        access_key: process.env.REACT_APP_API_KEY,
+                        query: `${country.capital}, ${country.name.common}`
+                    }
+                })
+                .then(response => {
+                    response.data.status === true ? setWeather(response.data) : setWeather(null)
+                }).catch((error) => {
+                    setWeather(null)
+                    console.error(error)
+                })
+    }, [country])
 
     return (
         <>
             {country
                 ? (
                     <>
-                        <h1>{country.name.common}</h1>
+                        <h3>{country.name.common}</h3>
                         <ul>
                             <p>Capital: {country.capital}</p>
                             <p>Population: {country.population}</p>
@@ -25,6 +45,15 @@ function Country({ country }) {
                             }
                         </ul>
                         <img src={country.flags.png} alt={country.flags.alt} />
+                        <h3>weather in {country.capital}</h3>
+                        {weather
+                            ? <>
+                                <p>Temperature: {weather.current.temperature}</p>
+                                <img src={weather.current.weather_icons[0]} alt='weather icon' />
+                            </>
+                            : <>There is not information available in this moment</>
+                        }
+
                     </>
                 )
                 : <p>There are not countries with that name</p>}
