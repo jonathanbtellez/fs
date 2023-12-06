d4a9581857fa5a5996bef59fcd9532ebfb769ffd
Jonathan Tellez <jonathanbtellez@gmail.com>
2023-12-05 22:23:12 -0500

    handle cors

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
@@ -91,6 +93,6 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log('server runnig in http://localhost:3001')
