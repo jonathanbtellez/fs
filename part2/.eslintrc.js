2f9c44af3ea9124aa1e1ee9d3ce3e35292809cd1
Jonathan Tellez <jonathanbtellez@gmail.com>
2024-01-11 22:15:22 -0500

    add e2e config

---
+++ part2/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "jest/globals": true,
+
+        "cypress/globals": true
+    },
+    "extends": [ 
+      // ...
+    ],
+    "parserOptions": {
+      // ...
+    },
+    "plugins": [
+        "react", "jest", "cypress"
+    ],
+    "rules": {
+      // ...
+    }
+}
---
+++ part2/cypress/e2e/note_app.cy.js
@@ -0,0 +1,15 @@
+describe('Note app', function() {
+
+  beforeEach(function() {
+    cy.visit('http://localhost:3000')
+  })
+
+  it('front page can be opened', function() {
+    cy.contains('Notes')
+    cy.contains('Note app, Department of Computer Science, University of Helsinki 2020')
+  })
+
+  it('login form can be opened', function() {
+    cy.contains('login').click()
+  })
+})
